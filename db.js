e2bb29296f2153f070d759ed72289eb0e7f62aae
Hammad Ahmed <hammadahmed682003@gmail.com>
2026-09-21 08:02:51 +0100

    Added the Database Path and Port .env Variable

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, 'database.db');
+const dbPath = process.env.DATABASE_PATH || path.join(__dirname, 'database.db');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Could not connect to database', err);
